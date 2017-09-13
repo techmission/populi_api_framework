90ecc556caa0a398dbe1a7d2d21190b63df5ad5f
Evan Donovan <evan@techmission.org>
2017-09-13 11:57:03 -0400

    Started unit test section, added PHP notices

---
+++ tests/tagger.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Unit tests for the tagger script */
+
+$base_path = dirname(__FILE__);
+
+// Require core includes
+require_once $base_path . '../includes.inc';
+
+// Require the settings and additional methods for the tagger script
+require_once $base_path . '/tagger/settings.php';
+require_once $base_path . '/tagger/tags.inc';
+
+
