bb8eba14161ebdad3f9d004b7a4d9cd36be765ab
Evan Donovan <outofegypt@gmail.com>
2017-09-13 13:04:25 -0400

    Update tests

--- tests/tagger.php
+++ tests/tagger.php
@@ -24,17 +24,30 @@ $populi->login();
 /* Tag methods */
 // See results of get_tags
 echo 'Get tags: ' . PHP_EOL;
-var_dump(get_tags());
+$tags = get_tags();
+var_dump($tags);
 echo PHP_EOL;
 
 // Set results of get_tag_by_name
 echo 'Get tag id by name: ' . PHP_EOL;
-var_dump(get_tag_id_by_name('Pell 16-17'));
+$tag_id = get_tag_id_by_name('Pell 16-17'));
+var_dump($tag_id);
 echo PHP_EOL;
 
-/* Populi methods */
-// TODO: Add the testing of the addTag and removeTag after
-// this one is confirmed to work
+* Populi methods */
+// this is confirmed in base case
 echo 'possibleDuplicatePeopleByName' . PHP_EOL;
 $students = $populi->getPossibleDuplicatePeopleByName('Evan', 'Donovan');
 var_dump($students);
+
+// student id of first one
+echo 'Student id ' . PHP_EOL;
+$student_id = $students[0]['id'];
+var_dump($student_id);
+echo PHP_EOL;
+
+// addTag
+$result = $populi->addTag($student_id, $tag_id);
+var_dump($result);
+
+// Do removeTag after I check on Populi itself
