36f028dc4e4a1b166e208ce0fb1da8c2e175b180
Evan Donovan <evan@techmission.org>
2018-07-09 20:47:23 -0400

    fixed constant definition logic

--- transcript-dl/main.php
+++ transcript-dl/main.php
@@ -5,16 +5,18 @@
 /* Require includes */
 $base_path = dirname(__FILE__);
 
-// Define a key for security
-KEY_VALUE = '7303081e-83d7-11e8-8ba0-f23c91e40cf2';
-
-// Get PDF transcript
-GET_PDF_TRANSCRIPT = TRUE;
-
 // Core includes
 require_once $base_path . '/../includes.inc';
 
-// Download transcript from $_GET parameters
+/* Script constants */
+
+// Define a key for security
+define('KEY_VALUE', '7303081e-83d7-11e8-8ba0-f23c91e40cf2');
+
+// Get PDF transcript
+define('GET_PDF_TRANSCRIPT', TRUE);
+
+/* Download transcript from $_GET parameters */
 if (isset($_GET['person_id']) && is_numeric($_GET['person_id']) && isset($_GET['key']) && $_GET['key'] = KEY_VALUE){
 	// call the wrapper function around Populi's get transcript function
 	$result = get_transcript($_GET['person_id'], GET_PDF_TRANSCRIPT);
