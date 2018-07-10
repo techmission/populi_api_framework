cfc332503d73da82baf2fc1729baf4fbaf2207d4
Evan Donovan <evan@techmission.org>
2018-07-09 21:20:05 -0400

    set headers for pdf download

--- transcript-dl/main.php
+++ transcript-dl/main.php
@@ -22,8 +22,18 @@ if (isset($_GET['person_id']) && is_numeric($_GET['person_id']) && isset($_GET['
     $populi = new Populi();
     // Use default login credentials
     $populi->login();
-    // call the wrapper function around Populi's get transcript function
+    // call the wrapper function around Populi's get transcript function to get transcript
 	$result = $populi->get_transcript($_GET['person_id'], GET_PDF_TRANSCRIPT);
+	
+	// Set headers for PDF download
+	// https://stackoverflow.com/questions/20080341
+	header('Content-Type: application/pdf');
+    header('Content-Length: '. strlen( $result ));
+    header('Content-disposition: inline; filename="9a.' . $_GET['lastname'] . '_' . $_GET['firstname'] . '_Transcript.pdf"');
+    header('Cache-Control: public, must-revalidate, max-age=0');
+    header('Pragma: public');
+    header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+    header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
 	// Return the PDF
 	print($result);
 }
