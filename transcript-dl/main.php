35332405d06889c74da406d0f898e29eb17f6ec4
Evan Donovan <evan@techmission.org>
2018-07-17 21:30:15 -0400

    updated to use new layout_id parameter for custom layouts

--- transcript-dl/main.php
+++ transcript-dl/main.php
@@ -8,10 +8,16 @@ $base_path = dirname(__FILE__);
 // Core includes
 require_once $base_path . '/../includes.inc';
 
-/* Script constants */
+/* Script settings - change for your Populi instance */
 
-// Get PDF transcript
-define('GET_PDF_TRANSCRIPT', TRUE);
+// Program ID for transcript:
+// Undergraduate program (in programs table)
+define('PROGRAM_ID', 25207);
+
+// Layout name for transcript:
+// Use getPrintLayouts method to find out a layout id
+// or select * from files where type = "PAGE_LAYOUT"
+define('TRANSCRIPT_LAYOUT_NAME', 'USDE Transcript');
 
 /* Download transcript from $_GET parameters */
 if (isset($_GET['person_id']) && is_numeric($_GET['person_id']) && isset($_GET['key']) && $_GET['key'] = GUID){
@@ -19,8 +25,19 @@ if (isset($_GET['person_id']) && is_numeric($_GET['person_id']) && isset($_GET['
     $populi = new Populi();
     // Use default login credentials
     $populi->login();
+	
+	// get the Populi print layout by name
+	$result = $populi->doTask('getPrintLayouts');
+	// iterate over the returned results
+	foreach($result->print_layout as $layout) {
+		if($layout->type == 'TRANSCRIPT' && $layout->name == TRANSCRIPT_LAYOUT_NAME) {
+			$layout_id = $layout->id;
+			break;
+		}
+	}
     // call the wrapper function around Populi's get transcript function to get transcript
-	$result = $populi->get_transcript($_GET['person_id'], GET_PDF_TRANSCRIPT);
+	$params = array('person_id' => $_GET['person_id'], 'pdf' => TRUE, 'layout_id' => $layout_id, 'program_id' => PROGRAM_ID, 'official' => TRUE);
+	$result = $populi->get_transcript($params);
 	
 	// Set headers for PDF download
 	// https://stackoverflow.com/questions/20080341
